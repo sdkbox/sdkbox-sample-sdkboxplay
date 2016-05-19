74f4ca0b14cb498d0fd9ad7068e548037428005f
hyperandroid <hyperandroid@gmail.com>
2016-05-19 22:01:58 +0200

    Update google play services lib.

---
+++ cpp/proj.android/libs/gps/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ibon/punchbox/test/tt/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/bin/AndroidManifest.xml : \
+/Users/ibon/punchbox/test/tt/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/AndroidManifest.xml \
