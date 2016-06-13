ea7c0c20faf86795264e15035d019a5ed11e176f
yinjimmy <jimmy.yin5@gmail.com>
2016-06-13 11:13:46 +0800

    remove unused files

--- cpp/proj.android/libs/gps/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ibon/punchbox/test/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/bin/AndroidManifest.xml : \
-/Users/ibon/punchbox/test/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/AndroidManifest.xml \
