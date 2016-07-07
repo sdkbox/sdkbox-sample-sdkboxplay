1b42318e7f102076a6a3c80dc42082d03948482d
hyperandroid <hyperandroid@gmail.com>
2016-07-07 17:14:32 +0200

    Test with changed login flow + connect_on_startup + connect_on_install

---
+++ cpp/proj.android/libs/gps/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ibon/punchbox/test/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/bin/AndroidManifest.xml : \
+/Users/ibon/punchbox/test/sdkbox-sample-sdkboxplay/cpp/proj.android/libs/gps/AndroidManifest.xml \
